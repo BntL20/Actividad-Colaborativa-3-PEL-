8a8896c612fd0475ca09300d2c096adecc8b5814
Bntt20 <eddybrent10@gmail.com>
2025-11-04 17:59:49 +0100

    Codigo incial del proyecto

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
+    cout << "Bienvenido al codigo compartido de PEL" <<endl;
     return 0;
 }
