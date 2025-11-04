fb270fae175190542ca31847912d56db54975205
luciasg9 <lucia2006sg@gmail.com>
2025-11-04 18:02:46 +0100

    Prueba Lucia

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ using namespace std;
 int main() {
     cout << "Hello, World!" << endl;
     cout << "Bienvenido al codigo compartido de PEL" <<endl;
+    cout << "Son las 18:02" << endl;
     return 0;
 }
